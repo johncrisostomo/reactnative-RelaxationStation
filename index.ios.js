cbcc230518ed5097e9b14c6bbe4ecb0846ea598c
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-21 15:14:22 +0800

    displays zen image as a button

--- index.ios.js
+++ index.ios.js
@@ -3,13 +3,20 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
+
+const zenImage = require('./assets/zen.png');
 
 export default class RelaxationStation extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={() => { alert('I was pressed!') }}>
+          <Image source={zenImage} style={styles.buttonImage} />
+        </TouchableOpacity>
         <Text style={styles.readyText}>I am ready to relax. . .</Text>
       </View>
     );
@@ -28,6 +35,20 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
     color: '#ffffff',
   },
+  button: {
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
+  buttonImage: {
+    width: 200,
+    height: 200,
+  }
 });
 
 AppRegistry.registerComponent('RelaxationStation', () => RelaxationStation);
